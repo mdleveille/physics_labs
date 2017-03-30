5f4fe5158836217c912a7edc81cc448bfe073c5d
Michael Leveille <leveille1313@gmail.com>
2017-03-30 13:37:53 -0600

    updated print lines and more

--- twoD_projectile.py
+++ twoD_projectile.py
@@ -2,13 +2,13 @@ import numpy as np
 
 g = 9.8   #m/s^2
 
-# del_y = float(input('del_y (in meters) = '))
-# theta = float(input('theta (in degrees) = '))
-# y0 = float(input('y0 (in meters) = '))
+del_y = float(input('del_y (in meters) = '))     # distance that the ball goes up minus launch position
+theta = float(input('theta (in degrees) = '))    # the angle you want to shoot the ball at
+#y0 = float(input('y0 (in meters) = '))           # height of launch position above table surface
 
-del_y = 1.16 # meters
-theta = 30   # degrees
-y0 = 0.25   # meters
+# del_y = 1.16 # meters
+# theta = 30   # degrees
+y0 = 0.265   # meters
 
 v0 = (2*g*del_y)**0.5
 #v0 = 4 #m/s 
@@ -21,6 +21,8 @@ c = y0
 
 t1 = (-b+np.sqrt(b**2-4*a*c))/(2*a)
 t2 = (-b-np.sqrt(b**2-4*a*c))/(2*a)
+print('time 1: %.2f seconds' % (t1))
+print('time 2: %.2f seconds' % (t2))
 
 x = v0x*t2
 print('the x distance is %.2f meters' % (x))
